3200b30b2d69c6fd0ff374ab1edf0f3a15dab0b6
jyothi72 <jyothilax72@gamil.com>
2023-03-21 14:08:35 +0530

    expense track first

---
+++ src/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>expenseitemmm!</h2>
+      <ul>food rs 10</ul>
+      <ul>petrol rs 100</ul>
+      <ul>food rs 10</ul>
+    </div>
+  )
+  
+}
+export default ExpenseItem;
